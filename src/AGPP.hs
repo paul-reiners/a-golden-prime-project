eec4236fd53626c84800880e17d7a3650396574f
Reiners, Paul <Paul.Reiners@Pearson.com>
2014-08-01 17:03:48 -0500

    Added timing code.

--- src/AGPP.hs
+++ src/AGPP.hs
@@ -4,6 +4,25 @@
 module AGPP where 
 
 import TUOLP
+import Text.Printf
+import Control.Exception
+import System.CPUTime
 
 getPrimePairs :: Integer -> [(Integer, Integer)]
 getPrimePairs n = [(p, q) | p <- takeWhile (< n) primes, q <- takeWhile (< n) primes, p <= q, p + q == n]
+
+-- See http://www.haskell.org/haskellwiki/Timing_computations
+time :: IO t -> IO t
+time a = do
+    start <- getCPUTime
+    v <- a
+    end   <- getCPUTime
+    let diff = (fromIntegral (end - start)) / (10^12)
+    printf "Computation time: %0.3f sec\n" (diff :: Double)
+    return v
+ 
+main = do
+    putStrLn "Starting..."
+    time $ getPrimePairs 256 `seq` return ()
+    putStrLn "Done."
+	
