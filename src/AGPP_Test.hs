e0f7cfd160d9733535e5ae243eebae38f1e13aea
Paul Reiners <paul.reiners@gmail.com>
2014-08-01 19:08:04 -0500

    Added a unit test.

--- src/AGPP_Test.hs
+++ src/AGPP_Test.hs
@@ -3,7 +3,10 @@ module AGPP_Test where
 import AGPP( getPrimePairs )
 import Test.HUnit
 
-testGetPrimePairs = TestCase $ assertEqual
+testGetPrimePairs26 = TestCase $ assertEqual
 	"getPrimePairs failed for n = 26" [(3,23),(7,19),(13,13)] ( getPrimePairs 26 )
+
+testGetPrimePairs100 = TestCase $ assertEqual
+	"getPrimePairs failed for n = 100" [(3,97),(11,89),(17,83),(29,71),(41,59),(47,53)] ( getPrimePairs 100 )
 	
-main = runTestTT $ TestList [testGetPrimePairs]
+main = runTestTT $ TestList [testGetPrimePairs26, testGetPrimePairs100]
