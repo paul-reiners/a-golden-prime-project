776e70d45a44f9bb50a16b08f7a738165fb28759
Paul Reiners <paul.reiners@gmail.com>
2014-08-02 09:17:23 -0500

    Adding more efficient version.

--- src/AGPP.hs
+++ src/AGPP.hs
@@ -5,5 +5,16 @@ module AGPP where
 
 import TUOLP
 
+getPrimePairs' :: Integer -> [(Integer, Integer)]
+getPrimePairs' n = [(p, q) | p <- takeWhile (< n) primes, q <- takeWhile (< n) primes, p <= q, p + q == n]
+
+getPrimePairsHelper :: Integer -> [Integer] -> [(Integer, Integer)]
+getPrimePairsHelper n ps 
+    | length ps == 0                           = []
+    | length ps == 1 && head ps + head ps == n = [(head ps, head ps)]
+    | head ps + last ps < n                    = getPrimePairsHelper n (tail ps)
+    | head ps + last ps > n                    = getPrimePairsHelper n (init ps)
+    | otherwise                                = (head ps, last ps) : getPrimePairsHelper n (tail (init ps))
+    
 getPrimePairs :: Integer -> [(Integer, Integer)]
-getPrimePairs n = [(p, q) | p <- takeWhile (< n) primes, q <- takeWhile (< n) primes, p <= q, p + q == n]
+getPrimePairs n = getPrimePairsHelper n (takeWhile (< n) primes)
