20c24abad8e5ed7a207d6eadc983798a010a7f91
Paul Reiners <paul.reiners@gmail.com>
2014-08-02 16:37:25 -0500

    More pair plotting.

--- src/R/prime_pair_count_graphs.R
+++ src/R/prime_pair_count_graphs.R
@@ -1,5 +1,6 @@
-prime.pair.count.1000 <- read.csv("PrimePairCounts1000.csv")
-scatter.smooth(prime.pair.count.1000$n, prime.pair.count.1000$prime.pair.count, 
+prime.pair.count <- read.csv("PrimePairCounts10000.csv")
+colnames(prime.pair.count) <- c("n", "prime.pair.count")
+scatter.smooth(prime.pair.count$n, prime.pair.count$prime.pair.count, 
                lpars = list(col = "blue", lwd = 3), 
                main="Number of prime pairs", xlab="n", 
                ylab="number of prime pairs")
