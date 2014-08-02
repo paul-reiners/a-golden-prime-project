afa52b160c2195f63ab3f0d48ef6885409cfad2b
Paul Reiners <paul.reiners@gmail.com>
2014-08-02 14:09:00 -0500

    Renaming.

--- src/AGPP.hs
+++ src/AGPP.hs
@@ -5,16 +5,16 @@ module AGPP where
 
 import TUOLP
 
-getPrimePairs' :: Integer -> [(Integer, Integer)]
-getPrimePairs' n = [(p, q) | p <- takeWhile (< n) primes, q <- takeWhile (< n) primes, p <= q, p + q == n]
+primePairs' :: Integer -> [(Integer, Integer)]
+primePairs' n = [(p, q) | p <- takeWhile (< n) primes, q <- takeWhile (< n) primes, p <= q, p + q == n]
 
-getPrimePairsHelper :: Integer -> [Integer] -> [(Integer, Integer)]
-getPrimePairsHelper n ps 
+primePairsHelper :: Integer -> [Integer] -> [(Integer, Integer)]
+primePairsHelper n ps 
     | length ps == 0                           = []
     | length ps == 1 && head ps + head ps == n = [(head ps, head ps)]
-    | head ps + last ps < n                    = getPrimePairsHelper n (tail ps)
-    | head ps + last ps > n                    = getPrimePairsHelper n (init ps)
-    | otherwise                                = (head ps, last ps) : getPrimePairsHelper n (tail (init ps))
+    | head ps + last ps < n                    = primePairsHelper n (tail ps)
+    | head ps + last ps > n                    = primePairsHelper n (init ps)
+    | otherwise                                = (head ps, last ps) : primePairsHelper n (tail (init ps))
     
-getPrimePairs :: Integer -> [(Integer, Integer)]
-getPrimePairs n = getPrimePairsHelper n (takeWhile (< n) primes)
+primePairs :: Integer -> [(Integer, Integer)]
+primePairs n = primePairsHelper n (takeWhile (< n) primes)
--- src/AGPP_Test.hs
+++ src/AGPP_Test.hs
@@ -1,12 +1,12 @@
 module AGPP_Test where
 
-import AGPP( getPrimePairs )
+import AGPP( primePairs )
 import Test.HUnit
 
-testGetPrimePairs26 = TestCase $ assertEqual
-	"getPrimePairs failed for n = 26" [(3,23),(7,19),(13,13)] ( getPrimePairs 26 )
+testPrimePairs26 = TestCase $ assertEqual
+	"primePairs failed for n = 26" [(3,23),(7,19),(13,13)] ( primePairs 26 )
 
-testGetPrimePairs100 = TestCase $ assertEqual
-	"getPrimePairs failed for n = 100" [(3,97),(11,89),(17,83),(29,71),(41,59),(47,53)] ( getPrimePairs 100 )
+testPrimePairs100 = TestCase $ assertEqual
+	"primePairs failed for n = 100" [(3,97),(11,89),(17,83),(29,71),(41,59),(47,53)] ( primePairs 100 )
 	
-main = runTestTT $ TestList [testGetPrimePairs26, testGetPrimePairs100]
+main = runTestTT $ TestList [testPrimePairs26, testPrimePairs100]
