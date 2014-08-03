2339eaede626dec14e2ac19e43092b931c8f1a23
Paul Reiners <paul.reiners@gmail.com>
2014-08-03 14:42:32 -0500

    Enlarging a plot.

--- src/R/prime_pair_count_graphs.R
+++ src/R/prime_pair_count_graphs.R
@@ -1,5 +1,8 @@
+max <- 1000
 prime.pair.count <- read.csv("PrimePairCounts10000.csv")
 colnames(prime.pair.count) <- c("n", "prime.pair.count")
+rows <- prime.pair.count$n <= max
+prime.pair.count <- prime.pair.count[rows,]
 scatter.smooth(prime.pair.count$n, prime.pair.count$prime.pair.count, 
                lpars = list(col = "blue", lwd = 3), 
                main="Number of prime pairs", xlab="n", 
