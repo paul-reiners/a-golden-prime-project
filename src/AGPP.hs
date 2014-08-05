858cb0c206489f726bfb9d0441c344d7e4e0b328
Reiners, Paul <Paul.Reiners@Pearson.com>
2014-08-05 15:41:24 -0500

    Hiding function.

--- src/AGPP.hs
+++ src/AGPP.hs
@@ -8,16 +8,15 @@ import TUOLP (primes)
 primePairs' :: Integer -> [(Integer, Integer)]
 primePairs' n = [(p, q) | p <- takeWhile (< n) primes, q <- takeWhile (< n) primes, p <= q, p + q == n]
 
-primePairsHelper :: Integer -> [Integer] -> [(Integer, Integer)]
-primePairsHelper n ps 
-    | length ps == 0                           = []
-    | length ps == 1 && head ps + head ps == n = [(head ps, head ps)]
-    | head ps + last ps < n                    = primePairsHelper n (tail ps)
-    | head ps + last ps > n                    = primePairsHelper n (init ps)
-    | otherwise                                = (head ps, last ps) : primePairsHelper n (tail (init ps))
-    
 primePairs :: Integer -> [(Integer, Integer)]
 primePairs n = primePairsHelper n (takeWhile (< n) primes)
+	where
+	primePairsHelper n ps 
+		| length ps == 0                           = []
+		| length ps == 1 && head ps + head ps == n = [(head ps, head ps)]
+		| head ps + last ps < n                    = primePairsHelper n (tail ps)
+		| head ps + last ps > n                    = primePairsHelper n (init ps)
+		| otherwise                                = (head ps, last ps) : primePairsHelper n (tail (init ps))
 
 primePairCount :: Integer -> Int
 primePairCount n = length (primePairs n)
