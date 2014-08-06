fe27a20999ef9ff40bbdecd199d43b88cc0f90da
Reiners, Paul <Paul.Reiners@Pearson.com>
2014-08-06 13:07:20 -0500

    Avoided code duplication.

--- src/AGPP.hs
+++ src/AGPP.hs
@@ -14,9 +14,10 @@ primePairs n = primePairsHelper n (takeWhile (< n) primes)
 	primePairsHelper n ps 
 		| length ps == 0                           = []
 		| length ps == 1 && head ps + head ps == n = [(head ps, head ps)]
-		| head ps + last ps < n                    = primePairsHelper n (tail ps)
-		| head ps + last ps > n                    = primePairsHelper n (init ps)
+		| headPlusLast < n                    = primePairsHelper n (tail ps)
+		| headPlusLast > n                    = primePairsHelper n (init ps)
 		| otherwise                                = (head ps, last ps) : primePairsHelper n (tail (init ps))
+		where headPlusLast = head ps + last ps
 
 primePairCount :: Integer -> Int
 primePairCount n = length (primePairs n)
